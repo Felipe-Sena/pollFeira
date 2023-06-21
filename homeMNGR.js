4423fb05863c2709323dd38aa7bbd65e76fe0049
Felipe <93457186+Felipe-Sena@users.noreply.github.com>
2023-06-20 23:06:11 -0300

    first commit

---
+++ homeMNGR.js
@@ -0,0 +1,10 @@
+/*
+------------------------------------------------------------------------------------------------------------
+    Copyright Felipe Sena | 2023 | Licenced under the GNU General Public Licence | Read the licence on the LICENCE.txt file
+------------------------------------------------------------------------------------------------------------
+*/
+
+function switchButtonMNGR(id) {
+    window.location.href=id;
+    console.log('foo');
+}
