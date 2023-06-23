576ec640d25d83cc0391b63f2e390000fe15697c
Felipe <93457186+Felipe-Sena@users.noreply.github.com>
2023-06-23 20:28:10 -0300

    Changed some stuff

--- localPollManager.js
+++ localPollManager.js
@@ -5,37 +5,92 @@
 */
 
 
-let yes = document.getElementById("btn0")
-let no = document.getElementById("btn1")
+let yesb = document.getElementById("btn0")
+let nob = document.getElementById("btn1")
 let thanksMate = document.getElementById("thanksMate")
+let lYes = 0
+let lNo = 0
 
-if (!localStorage.localYes) {
-    localStorage.localYes = 0
-}
-if (!localStorage.localNo) {
-    localStorage.localNo = 0
-}
 
-function submit(choice)
+const firebaseConfig = {
+
+    // GHUB secret?
+    apiKey: "AIzaSyCBSAoSHq0PTORSpGHANTvAhWEuSJXr4cM",
+
+    authDomain: "pollfeirapub.firebaseapp.com",
+
+    databaseURL: "https://pollfeirapub-default-rtdb.firebaseio.com",
+
+    projectId: "pollfeirapub",
+
+    storageBucket: "pollfeirapub.appspot.com",
+
+    messagingSenderId: "254381617251",
+
+    appId: "1:254381617251:web:b0529533e2aa1a3754c856",
+
+    measurementId: "G-JW6EE1M0DY"
+
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  const db = firebase.database();
+  const mainRef = db.ref("votes")
+  
+
+  function submit(choice)
 {
     switch(choice)
     {
         case 'Y':
             {
-                localStorage.localYes = parseInt(localStorage.localYes) + 1
+                lYes++
+                db.ref("votes/lYes").set(
+                    {
+                        Type:"lYes",
+                        Value:lYes
+                    }
+                )
                 hideSubmission()
                 break;
             }
         case 'N': 
             {
-                localStorage.localNo = parseInt(localStorage.localNo) + 1
+                lNo++
+                db.ref("votes/lNo").set(
+                    {
+                        Type:"lNo",
+                        Value:lNo
+                    }
+                )
                 hideSubmission()
                 break;
             } 
     }
 }
 
+mainRef.on('child_changed', (snapshot) => {
+    const newPost = snapshot.val();
+    if(newPost.Type == "Yes" || newPost.Type == "No")
+    {
+        return;
+    }
+    let valu = newPost.Value;
+   // console.log(valu);
+    eval(newPost.Type + "=" + valu);
+});
+
 function hideSubmission()
 {
  thanksMate.style.display = "block"   
 }
+
+mainRef.on('child_added', (snapshot) => {
+    const newPost = snapshot.val();
+    if(newPost.Type == "Yes" || newPost.Type == "No")
+    {
+        return;
+    }
+    let valu = newPost.Value;
+    eval(newPost.Type + "=" + valu); //this is so fire trucking weird
+});
